66a2a232254dcf31bbbb7cea63c618fac8880f3a
James Kua <kua.james30@gmail.com>
2024-01-20 11:51:54 +0800

    Add basic destinations table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx"
-
+import Destinations from './components/Destionations.jsx';
 
 function App() {
 
@@ -12,6 +12,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path='/home' element={<Dashboard />}/>
+                <Route path='/destinations' element={<Destinations />}/>
             </Routes>
         </BrowserRouter>
     </>
