082a30f70a2fb1e10586c54035a62210919c62ab
Denise Lam <e0564445@u.nus.edu>
2024-01-20 12:03:06 +0800

    reviewed warnings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx"
 import Destinations from './components/Destinations.jsx';
